eaac39c61eb5c7072a1907679ef69a4d8a3775b6
afamaria <marie.afanasyeva@gmail.com>
2024-09-17 11:45:42 +0500

    Feat: add svg loader plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
-  base: "/mediacube_todo/",
+  plugins: [vue(), vueJsx(), vueDevTools(), svgLoader()],
+  base: '/mediacube_todo/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
