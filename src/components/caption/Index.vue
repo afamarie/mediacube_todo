644820a110ea68ccda5eb23e4fb2f9adc1c90589
afamaria <marie.afanasyeva@gmail.com>
2024-09-19 22:32:22 +0500

    Feat: add captopm

---
+++ src/components/caption/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <p class="caption">
+    <CheckMarkIcon aria-hidden="true" />
+    <span>Congrats, you have no more tasks to do</span>
+  </p>
+</template>
+
+<script setup lang="ts">
+import CheckMarkIcon from '@/assets/icons/checkmark-round.svg'
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/styles/mixins.scss';
+
+.caption {
+  @include body14;
+
+  text-align: center;
+  color: var(--text-neutral);
+
+  svg {
+    width: 1.5rem;
+    height: 1.5rem;
+    margin-right: 0.5rem;
+    vertical-align: middle;
+  }
+}
+</style>
